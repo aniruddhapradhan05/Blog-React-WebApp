3531e74c7ca83897af85110f2f82286626ef0542
Aniruddha Pradhan <104893063+aniruddhapradhan05@users.noreply.github.com>
2024-03-04 18:50:50 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,40 +12,56 @@ export default function RTE({name, control, label, defaultValue =""}) {
     name={name || "content"}
     control={control}
     render={({field: {onChange}}) => (
-        <Editor
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
+        // <Editor
+        // initialValue={defaultValue}
+        // init={{
+        //     initialValue: defaultValue,
+        //     height: 500,
+        //     menubar: true,
+        //     plugins: [
+        //         "image",
+        //         "advlist",
+        //         "autolink",
+        //         "lists",
+        //         "link",
+        //         "image",
+        //         "charmap",
+        //         "preview",
+        //         "anchor",
+        //         "searchreplace",
+        //         "visualblocks",
+        //         "code",
+        //         "fullscreen",
+        //         "insertdatetime",
+        //         "media",
+        //         "table",
+        //         "code",
+        //         "help",
+        //         "wordcount",
+        //         "anchor",
+        //     ],
+        //     toolbar:
+        //     "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+        //     content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+        // }}
+        // onEditorChange={onChange}
+        // />
+      <Editor
+      apiKey='wlqmlx05grk3laalbx5z8dz0kbvu3d346kyru26rp0bt8ghv'
+      init={{
+        plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        tinycomments_mode: 'embedded',
+        tinycomments_author: 'Author name',
+        mergetags_list: [
+          { value: 'First.Name', title: 'First Name' },
+          { value: 'Email', title: 'Email' },
+        ],
+        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+      }}
+      initialValue="Welcome to TinyMCE!"
+      onEditorChange={onChange}
+    />
     )}
     />
 
